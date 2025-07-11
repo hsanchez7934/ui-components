f848757cba217500e3d017b464b61b4e757c4831
Hector A. Sanchez <hsanchez7934@gmail.com>
2025-07-10 19:32:29 -0600

    update checkbox component to include checkbox groups

--- src/app/checkbox/page.tsx
+++ src/app/checkbox/page.tsx
@@ -2,13 +2,40 @@
 
 import Checkbox from '@/app/components/checkbox/Checkbox'
 import PageMainBox from '../components/page-main-box/PageMainBox'
+import CheckboxGroup from '../components/checkbox/CheckboxGroup'
+
+const meats = [
+	{value: 'bacon', label: 'Bacon'},
+	{value: 'ham', label: 'Ham'},
+	{value: 'pepperoni', label: 'Pepperoni'},
+	{value: 'salami', label: 'Salami'}
+]
+
+const veggies = [
+	{value: 'olives', label: 'Olives'},
+	{value: 'onions', label: 'Onions'},
+	{value: 'mushrooms', label: 'Mushrooms'},
+	{value: 'peppadews', label: 'Peppadews'}
+]
 
 export default function CheckboxPage() {
 	return (
 		<PageMainBox>
-			<Checkbox index={0} />
-			<Checkbox index={1} />
-			<Checkbox index={2} />
+			<div className="h-auto w-full xl:flex">
+				<section className="flex items-center justify-center flex-col p-7 xl:p-20">
+					<Checkbox index={0} />
+					<Checkbox index={1} />
+					<Checkbox index={2} />
+				</section>
+				<section className="flex items-center justify-center flex-col p-7 xl:p-20">
+					<div className="mb-7">
+						<CheckboxGroup parentLabel="Meat" options={meats} />
+					</div>
+					<div>
+						<CheckboxGroup parentLabel="Vegetables" options={veggies} />
+					</div>
+				</section>
+			</div>
 		</PageMainBox>
 	)
 }
--- src/app/components/checkbox/Checkbox.tsx
+++ src/app/components/checkbox/Checkbox.tsx
@@ -10,6 +10,7 @@ interface Props {
 const Checkbox = (props: Props) => {
 	const [checked, setChecked] = React.useState(false)
 	const [checkboxText, setCheckboxText] = React.useState('')
+	const [isIndeterminate, setIsIndeterminate] = React.useState(false)
 	const [index, setIndex] = React.useState(props.index || 0)
 	const inputRef = React.useRef<HTMLInputElement>(null)
 
@@ -17,16 +18,19 @@ const Checkbox = (props: Props) => {
 		if (index === 0) {
 			setCheckboxText('Unchecked')
 			setChecked(false)
+			setIsIndeterminate(false)
 		}
 		if (index === 1) {
 			setCheckboxText('Indeterminate')
 			// @ts-expect-error generic
 			inputRef.current.indeterminate = true
 			setChecked(false)
+			setIsIndeterminate(true)
 		}
 		if (index === 2) {
 			setCheckboxText('Checked')
 			setChecked(true)
+			setIsIndeterminate(false)
 		}
 	}, [])
 
@@ -37,6 +41,7 @@ const Checkbox = (props: Props) => {
 			input.indeterminate = true
 			setCheckboxText('Indeterminate')
 			setIndex(1)
+			setIsIndeterminate(true)
 		}
 		if (index === 1) {
 			// @ts-expect-error generic
@@ -44,11 +49,13 @@ const Checkbox = (props: Props) => {
 			setChecked(true)
 			setCheckboxText('Checked')
 			setIndex(2)
+			setIsIndeterminate(false)
 		}
 		if (index === 2) {
 			setChecked(false)
 			setCheckboxText('Unchecked')
 			setIndex(0)
+			setIsIndeterminate(false)
 		}
 	}
 
@@ -62,7 +69,7 @@ const Checkbox = (props: Props) => {
 				ref={inputRef}
 				checked={checked}
 				onChange={handleOnChange}
-				className="w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8 lg:w-9 lg:h-9 cursor-pointer"
+				className={`custom-checkbox ${isIndeterminate ? 'indeterminate' : ''} w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8 lg:w-9 lg:h-9 cursor-pointer`}
 			/>
 			<span className="text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl w-28">
 				{checkboxText}
