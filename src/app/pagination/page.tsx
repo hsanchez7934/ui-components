cdc0bc88a0af94a9bbb8bac43911a0b8ac0f1934
Hector A. Sanchez <hsanchez7934@gmail.com>
2025-07-10 22:28:13 -0600

    add pagination component

--- src/app/pagination/page.tsx
+++ src/app/pagination/page.tsx
@@ -1,10 +1,21 @@
+'use client'
+
+import React from 'react'
 import PageMainBox from '../components/page-main-box/PageMainBox'
-import PaginationComponent from '../components/pagination/Pagination'
+import Pagination from '../components/pagination/Pagination'
 
 export default function PaginationPage() {
+	const [page, setPage] = React.useState(1);
+
 	return (
 		<PageMainBox>
-			<PaginationComponent />
+			<Pagination
+				totalPages={10}
+				currentPage={page}
+				onPageChange={(newPage) => setPage(newPage)}
+				prevLabel="Previous"
+				nextLabel="Next"
+			/>
 		</PageMainBox>
 	)
 }
