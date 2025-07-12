51c69b558c558d4443d86a18172dda7c32c641fb
Hector A. Sanchez <hsanchez7934@gmail.com>
2025-07-11 18:12:49 -0600

    mc

--- src/app/components/page-main-box/PageMainBox.tsx
+++ src/app/components/page-main-box/PageMainBox.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function PageMainBox(props: Props) {
 	return (
-		<main className="min-h-screen pt-30 xl:pt-0 bg-white dark:bg-black flex flex-col items-center justify-start xl:justify-center">
+		<main className="min-h-screen pt-30 bg-white dark:bg-black flex flex-col items-center justify-start xl:justify-center">
 			<div className="h-auto">
 				{props.children}
 			</div>
